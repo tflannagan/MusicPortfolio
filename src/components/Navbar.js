79bc391c6ba9977664dcae418a7d207ce66b0927
Ty Flannagan <tylerflannagan@yahoo.com>
2023-10-24 13:23:15 -0700

    deploy

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./Navbar.css";
+import Logo from "../assets/avatar.png";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="nav-logo">O_I</div>
+      <div className="nav-logo">
+        <a href="https://soundcloud.com/igniculus_o">
+          {" "}
+          <img src={Logo} />
+        </a>
+      </div>
       <ul className="nav-links">
         <li>
           <a href="#home">Home</a>
--- src/components/Songs.js
+++ src/components/Songs.js
@@ -37,33 +37,15 @@ export default function Songs() {
         ></iframe>
       </div>
 
-      {/* <iframe
-        width="100%"
-        height="300"
-        scrolling="no"
-        frameborder="no"
-        allow="autoplay"
-        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1494911452&color=%232f444b&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-      ></iframe>
-      <div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">
-        <a
-          href="https://soundcloud.com/igniculus_o"
-          title="O_I"
-          target="_blank"
-          style="color: #cccccc; text-decoration: none;"
-        >
-          O_I
-        </a>{" "}
-        ·{" "}
-        <a
-          href="https://soundcloud.com/igniculus_o/continuum"
-          title="Continuum"
-          target="_blank"
-          style="color: #cccccc; text-decoration: none;"
-        >
-          Continuum
-        </a>
-      </div> */}
+      <div className="song">
+        <iframe
+          width="100%"
+          title="OI"
+          height="300"
+          allow="autoplay"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1505603623%3Fsecret_token%3Ds-jfQ76koeoVb&color=%232f444b&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+        ></iframe>
+      </div>
     </section>
   );
 }
